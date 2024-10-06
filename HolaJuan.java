78c9f7343ea1c71ee6a37fa546cc9cb2678d863f
Juan Cristian Llobera Vázquez <jlloberavazquez@cifpfbmoll.eu>
2024-10-06 23:05:45 +0200

    HolaJuan.java

--- HolaJuan.java
+++ HolaJuan.java
@@ -1,4 +1,4 @@
-public class HolaJuan {
+public class HolaJuanLlobera {
     public static void main(String[] args) {
         System.out.println("Hola, me llamo Juan Cristian Llobera Vázquez");
     }
